b5685924e0f8d65169044db57e829ae6247ca382
milchmek <pongkhun.mek@gmail.com>
2024-10-18 18:15:25 +0700

    set the window can draw or add anything

--- src/App.java
+++ src/App.java
@@ -6,10 +6,15 @@ public class App {
         int boardHeight = 640; // set height of the window
 
         JFrame frame = new JFrame("Flappy Bird"); // set the title
-        frame.setVisible(true); // set show on the window
+//        frame.setVisible(true); // set show on the window
         frame.setSize(boardWidth, boardHeight); // set width and height
         frame.setLocationRelativeTo(null); // set display on center
         frame.setResizable(false); // set cannot resize the window
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // set when close the game, the game will exit.
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // set when close the game, the game will exit
+
+        FlappyBird flappyBird = new FlappyBird();
+        frame.add(flappyBird);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
