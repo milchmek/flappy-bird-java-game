80d9df4daa0eb23a31710deb06d934ff98824483
milchmek <pongkhun.mek@gmail.com>
2024-10-18 18:28:47 +0700

    set background image

--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -8,8 +8,30 @@ public class FlappyBird extends JPanel{
     int boardWidth = 360;
     int boardHeight = 640;
 
+    // images
+    Image backGroundImg;
+    Image birdImg;
+    Image topPipeImg;
+    Image bottomPipeImg;
+
     FlappyBird() {
         setPreferredSize(new Dimension(boardWidth, boardHeight)); // set preferred size to board width and height
-        setBackground(Color.blue); // set background color
+//        setBackground(Color.blue); // set background color
+
+        // load images
+        backGroundImg = new  ImageIcon(getClass().getResource("./flappybirdbg.png")).getImage();
+        birdImg = new ImageIcon(getClass().getResource("./flappybird.png")).getImage();
+        topPipeImg = new ImageIcon(getClass().getResource("./toppipe.png")).getImage();
+        bottomPipeImg = new ImageIcon(getClass().getResource("./bottompipe.png")).getImage();
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g) {
+        // set the background Image
+        g.drawImage(backGroundImg, 0, 0, boardWidth, boardHeight, null);
     }
 }
